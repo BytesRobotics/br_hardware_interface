388f971a97eb2093f2503c6f1ff7ddbd1fd83a68
Michael Equi <equim20@student.jhs.net>
2020-06-01 21:13:52 -0700

    Added faster reading

--- scripts/TSS_Test/TSS_Test.ino
+++ scripts/TSS_Test/TSS_Test.ino
@@ -1,53 +1,67 @@
-#include <MS5xxx.h>
 #include <Wire.h>
 
+#define MS5xxx_CMD_RESET    0x1E    // perform reset
+#define MS5xxx_CMD_ADC_READ 0x00    // initiate read sequence
+#define MS5xxx_CMD_ADC_CONV 0x40    // start conversion
+#define MS5xxx_CMD_ADC_D1   0x00    // read ADC 1
 #define MS5xxx_CMD_ADC_256  0x00    // set ADC oversampling ratio to 256
 
-class MS560702: public MS5xxx{
-public:
-    MS560702(char aAddr) : MS5xxx(&Wire){
-      setI2Caddr(aAddr);
-    }
-    
-    void LightRead() {
-      unsigned long D1=0;
-      D1=read_adc(MS5xxx_CMD_ADC_D1+MS5xxx_CMD_ADC_256);
-      P=D1;
-    }
+void send_cmd(char addr, byte aCMD)
+{
+  Wire.beginTransmission(addr);
+  Wire.write(aCMD);
+  Wire.endTransmission(true);
+}
 
-};
+bool sensor_connect(char addr){
+  Wire.begin();
+  Wire.beginTransmission(addr);
+  return Wire.endTransmission(true);
+}
 
+void read_sensor_value(char addr, unsigned long& value){
+  Wire.requestFrom(addr, 3);
+  value = (Wire.read()<<16);
+  value = value | (Wire.read()<<8);
+  value = value | Wire.read();
+  Wire.endTransmission(true);
+}
 
-MS560702 sensor_l(0x77);
-MS560702 sensor_r(0x76);
+bool init_sensor(char addr){
+  if(sensor_connect(addr)){
+    send_cmd(addr, MS5xxx_CMD_RESET);
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 void setup() {
   Serial.begin(115200);
-
   while(!Serial);
-  
-  if(sensor_l.connect()>0) {
-    Serial.println("Error connecting...");
-    delay(500);
-  } else {
-    sensor_l.ReadProm();
-  }
 
-  if(sensor_r.connect()>0) {
-    Serial.println("Error connecting...");
-    delay(500);
-  } else {
-    sensor_r.ReadProm();
-  }
+  // Connect to sensor
+  init_sensor(0x77);
+  init_sensor(0x76);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sensor_l.LightRead();
-  sensor_r.LightRead();
-  Serial.print(sensor_l.GetPres());
-  Serial.print(" ");
-  Serial.println(sensor_r.GetPres());
 
+  unsigned long value_l=0, value_r=0;
+
+  send_cmd(0x76, MS5xxx_CMD_ADC_CONV + MS5xxx_CMD_ADC_D1 + MS5xxx_CMD_ADC_256);
+  send_cmd(0x77, MS5xxx_CMD_ADC_CONV + MS5xxx_CMD_ADC_D1 + MS5xxx_CMD_ADC_256);
+  delayMicroseconds(300);
+  send_cmd(0x76, MS5xxx_CMD_ADC_READ); // read out values
+  send_cmd(0x77, MS5xxx_CMD_ADC_READ); // read out values
+
+  read_sensor_value(0x77, value_l);
+  read_sensor_value(0x76, value_r);
+
+  
+  Serial.print(value_l);
+  Serial.print(" ");
+  Serial.println(value_r);
 }
